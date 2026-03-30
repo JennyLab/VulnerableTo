9ccecc9acb8fd4eee85020208ff5e82968e16d25
h0ffy <45763853+h0ffy@users.noreply.github.com>
2026-03-31 00:50:24 +0200

    Implement UpdateUserDto and updateProfile method
    Add UpdateUserDto class and updateProfile method with vulnerability note.

---
+++ nodejs/nestjs/vulnerable004.js
@@ -0,0 +1,30 @@
+/*
+  NestJS: Mass Aligment and Type Bypass
+
+
+{
+  "username": "h0ffy",
+  "email": "h0ffy@jl4b.net",
+  "role": "admin",
+  "isPremium": true
+}
+
+*/
+
+import { IsString, IsEmail } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  username: string;
+
+  @IsEmail()
+  email: string;
+}
+
+// Controller vulnerable
+@Patch('profile')
+async updateProfile(@Body() updateDto: UpdateUserDto) {
+  // Vulnerabilidad: Se pasan todas las propiedades de 'body' aunque no estén en el DTO
+  // si el ValidationPipe no tiene 'whitelist: true'
+  return await this.userService.update(this.currentUser.id, updateDto);
+}
